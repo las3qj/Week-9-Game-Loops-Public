61a59096ea3966aace3e3532fc5aa8e99bc6c969
las3qj <las3qj@virginia.edu>
2022-10-21 12:32:12 -0400

    Finished!

--- Assets/Learning Units/Unit 03/Scripts/MovementWatcher.cs
+++ Assets/Learning Units/Unit 03/Scripts/MovementWatcher.cs
@@ -17,22 +17,22 @@ public class MovementWatcher : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        moveOneByOne.MoveNextObject();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnEnable()
     {
-        
+
     }
 
     void OnDisable()
     {
+        moveOneByOne.MoveNextObject();
 
     }
     void OnApplicationQuit()
